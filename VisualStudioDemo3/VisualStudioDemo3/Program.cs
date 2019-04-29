077723e33d107673f3bc6a95591b3f0990d737e8
qhafeez26 <qhafeez26@yahoo.com>
2019-04-29 15:38:02 -0700

    changed writeline method and added readline

--- VisualStudioDemo3/VisualStudioDemo3/Program.cs
+++ VisualStudioDemo3/VisualStudioDemo3/Program.cs
@@ -6,7 +6,8 @@ namespace VisualStudioDemo3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Visual Studio Demo");
+            Console.ReadLine();
         }
     }
 }
